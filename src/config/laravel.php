2ef564f5c53382fa488d3ee9a5aba04edcb261ba
David Lartey <dbilovd@users.noreply.github.com>
2020-08-28 14:10:04 +0000

    Apply fixes from StyleCI

--- src/config/laravel.php
+++ src/config/laravel.php
@@ -3,14 +3,14 @@
 use Dbilovd\PHP_USSD\Screens\Home;
 
 return [
-	/**
-	 * The default USSD Gateway Provider to our application
-	 */
+    /**
+     * The default USSD Gateway Provider to our application.
+     */
     'defaultServiceProvider'	=> env('DEFAULT_USSD_PROVIDER', 'general'),
 
     /**
-     * The entry screen for the application
+     * The entry screen for the application.
      */
     'initialScreenClass'       	=> env('PHP_USSD_INITIAL_SCREEN', Home::class),
-    
+
 ];
