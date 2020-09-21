b99eb886f54351c054e10a8eedb15e841f96609f
David Lartey <dbilovd@gmail.com>
2020-09-21 18:55:17 +0000

    config: Setting defaultSessionManager to config file

--- src/config/laravel.php
+++ src/config/laravel.php
@@ -9,6 +9,12 @@ return [
     'defaultServiceProvider'	=> env('DEFAULT_USSD_PROVIDER', 'general'),
 
     /**
+     * Set the default method of saving and managing cache data
+     * Options: laravel-cache (default), redis
+     */
+    'defaultSessionManager'		=< env('DEFAULT_USSD_SESSION_MANAGER', 'laravel-cache'),
+
+    /**
      * The entry screen for the application.
      */
     'initialScreenClass'       	=> env('PHP_USSD_INITIAL_SCREEN', Home::class),
