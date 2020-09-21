86ca7dce9e872ca04509806ee87d0e3b58fa7683
David Lartey <dbilovd@gmail.com>
2020-09-21 21:18:54 +0000

    fix: syntax error

--- src/config/laravel.php
+++ src/config/laravel.php
@@ -12,7 +12,7 @@ return [
      * Set the default method of saving and managing cache data
      * Options: laravel-cache (default), redis
      */
-    'defaultSessionManager'		=< env('DEFAULT_USSD_SESSION_MANAGER', 'laravel-cache'),
+    'defaultSessionManager'		=> env('DEFAULT_USSD_SESSION_MANAGER', 'laravel-cache'),
 
     /**
      * The entry screen for the application.
